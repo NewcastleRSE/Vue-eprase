bcae0fd65688f44b39514903495c6f0fdc1ebd8e
rosselton <rebecca.osselton@ncl.ac.uk>
2019-08-28 13:18:43 +0100

    Various changes.

--- src/components/Patients.vue
+++ src/components/Patients.vue
@@ -12,7 +12,9 @@
           <strong>Gender</strong>
         </td>
       </tr>
-    <tr v-for="patient in patientList">
+
+
+    <tr v-for="patient in myPatientList">
       <td>
         {{patient.first_name}} {{patient.surname}}
       </td>
@@ -29,7 +31,8 @@
 </template>
 
 <script>
-    import json from '../json/patients.json'
+
+  import { patientService } from '../services/patient.service';
 
     export default {
         name: "Patients",
@@ -37,77 +40,22 @@
         },
         data() {
             return {
-                patients : json,
-                numPatients : 0,
-                patientList : [],
-                patientIds : []
+                patients : [],
+                myPatientList : []
             }
         },
         methods: {
-            setPatients: function() {
-
-               this.numPatients = this.patients.length;
-               let patientIndex = [];
-
-                // create a list of 10 patient indexes randomly from all patients
-                for(let i = 0; i < this.numPatients; i++){
-                    let index = Math.floor(Math.random() * this.numPatients);
-                    if(patientIndex.length < 10){
-                        // don't want the same patient twice
-                        if (!(patientIndex.includes(index))) {
-                            patientIndex.push(index);
-                        }
-                    }
+            getPatients() {
+                this.patients = patientService.getPatients();
+                // format : patientList[0] etc
+                for (let i = 0; i < this.patients.patientList.length; i++) {
+                    this.myPatientList.push(this.patients.patientList[i])
                 }
-
-                // loop through indexes and set the patient list and patient id list
-                for(let i = 0; i < patientIndex.length; i++) {
-                    this.patientList.push(this.patients[patientIndex[i]]);
-                    let patient = this.patients[patientIndex[i]];
-                    this.patientIds.push(patient.id);
-                }
-
-                // set the data in the central store
-                const patientList = this.patientList;
-                const patientIds = this.patientIds;
-                this.$store.dispatch('setPatientList', { patientList, patientIds });
-
-                // fix patient DOBs
-                for(let i = 0; i < this.patientList.length; i++) {
-                    this.patientList[i].dob = this.getDOB(this.patientList[i]);
-                }
-            },
-            getDOB(patient)
-            {
-                // get today's date
-                let today = new Date();
-
-                //randomise the date within the last 12 months
-                let days = Math.random() * 360;
-                today.setDate(today.getDate()-days);
-
-                // get date string info
-                let d = today.getDate();
-                let m = today.getMonth() + 1;
-                let yyyy = today.getFullYear();
-
-                // subtract patient's age
-                yyyy = yyyy - patient.age;
-
-                let dd = d.toString();
-                let mm = m.toString();
-
-                if (d < 10) {
-                    dd = '0' + d;
-                }
-                if (m < 10) {
-                    mm = '0' + m;
-                }
-                return dd + '/' + mm + '/' + yyyy;
             }
         },
-        created: function() {
-            this.setPatients();
+        created() {
+            this.getPatients();
+
         }
     }
 </script>
