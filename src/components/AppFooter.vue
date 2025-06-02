68e373340d06b05524317c8380b696f5e05a43f9
David Herbert <david.herbert2@newcastle.ac.uk>
2025-06-02 13:19:06 +0100

    Responsive button captions/sizes

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,32 +1,32 @@
 <template>
   <Vueform ref="footerButtonBar">
-    <GroupElement name="buttonBar" :columns="8" :add-class="'mt-2'">
-      <ButtonElement name="about" full
+    <GroupElement name="buttonBar" :columns="12" :add-class="'mt-2'">
+      <ButtonElement name="about" title="About" full
         data-bs-toggle="modal" data-bs-target="#aboutModal"
-        :columns="2" 
+        :columns="3" 
         :add-class="'me-2'">
-        <i class="bi bi-info-circle me-2"></i>About
+        <i class="bi bi-info-circle me-lg-2"></i><span class="d-lg-block d-sm-none d-none">About</span>
       </ButtonElement>
       <ButtonElement name="instructions" full 
         data-bs-toggle="modal" data-bs-target="#instructionsModal"
-        :columns="2" 
+        :columns="3" 
         :add-class="'mx-2'">
-        <i class="bi bi-clipboard me-2"></i>Instructions
+        <i class="bi bi-file-earmark-text me-lg-2"></i><span class="d-lg-block d-sm-none d-none">Instructions</span>
       </ButtonElement>      
       <ButtonElement name="userGuide" full 
         button-type="anchor"
         href="https://eprasedocs.blob.core.windows.net/web/Eprase2024UserGuide.pdf"
         target="_blank"
         title="PDF - opens in a new tab"
-        :columns="2" 
+        :columns="3" 
         :add-class="'mx-2'">
-        <i class="bi bi-question-circle me-2"></i>User guide
+        <i class="bi bi-question-circle me-lg-2"></i><span class="d-lg-block d-sm-none d-none">User guide</span>
       </ButtonElement>
       <ButtonElement name="contact" full 
         data-bs-toggle="modal" data-bs-target="#contactModal"
-        :columns="2" 
+        :columns="3"
         :add-class="'ms-2'">
-        <i class="bi bi-person-circle me-2"></i>Contact
+        <i class="bi bi-person-circle me-lg-2"></i><span class="d-lg-block d-sm-none d-none">Contact</span>
       </ButtonElement>
     </GroupElement>
   </Vueform>  
