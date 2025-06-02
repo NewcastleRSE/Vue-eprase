58a2556cc674a1d8a6c7c198a802ecccf479ed8a
David Herbert <david.herbert2@newcastle.ac.uk>
2025-06-02 15:09:13 +0100

    Button responsiveness corrected

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -5,28 +5,28 @@
         data-bs-toggle="modal" data-bs-target="#aboutModal"
         :columns="3" 
         :add-class="'me-2'">
-        <i class="bi bi-info-circle me-lg-2"></i><span class="d-lg-block d-sm-none d-none">About</span>
+        <i class="bi bi-info-circle me-lg-2"></i><span class="d-lg-block d-none">About</span>
       </ButtonElement>
-      <ButtonElement name="instructions" full 
+      <ButtonElement name="instructions" title="Instructions" full 
         data-bs-toggle="modal" data-bs-target="#instructionsModal"
         :columns="3" 
         :add-class="'mx-2'">
-        <i class="bi bi-file-earmark-text me-lg-2"></i><span class="d-lg-block d-sm-none d-none">Instructions</span>
+        <i class="bi bi-file-earmark-text me-lg-2"></i><span class="d-lg-block d-none">Instructions</span>
       </ButtonElement>      
       <ButtonElement name="userGuide" full 
         button-type="anchor"
         href="https://eprasedocs.blob.core.windows.net/web/Eprase2024UserGuide.pdf"
         target="_blank"
-        title="PDF - opens in a new tab"
+        title="User Guide as PDF - opens in a new tab"
         :columns="3" 
         :add-class="'mx-2'">
-        <i class="bi bi-question-circle me-lg-2"></i><span class="d-lg-block d-sm-none d-none">User guide</span>
+        <i class="bi bi-question-circle me-lg-2"></i><span class="d-lg-block d-none">User guide</span>
       </ButtonElement>
-      <ButtonElement name="contact" full 
+      <ButtonElement name="contact" title="Contact details" full 
         data-bs-toggle="modal" data-bs-target="#contactModal"
         :columns="3"
         :add-class="'ms-2'">
-        <i class="bi bi-person-circle me-lg-2"></i><span class="d-lg-block d-sm-none d-none">Contact</span>
+        <i class="bi bi-person-circle me-lg-2"></i><span class="d-lg-block d-none">Contact</span>
       </ButtonElement>
     </GroupElement>
   </Vueform>  
