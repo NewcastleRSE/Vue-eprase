7708627b8c18b8cf664b4068fff00101d82f15f7
John Schoneboom <john.schoneboom@gmail.com>
2025-10-06 09:06:32 +0100

    Minor fix for patient age display
    On the scenarios page, the patient name was followed by all three
    age types, rather than just the relevant one for the patient.
    This was happening because the code was testing for !=0 but
    in the database the empty field was null rather than 0.
    
    So this commit just changes !=0 to >0 and now it displays as
    expected.

--- src/components/AssessmentScenario.vue
+++ src/components/AssessmentScenario.vue
@@ -52,9 +52,9 @@
                     <img class="img-thumbnail" style="width: 50px; height: 50px" v-show="patient.gender === 'Female'"
                       src="../assets/images/anon-female.png" alt="female-patient" />
                     Patient: {{ patient.full_name }}, 
-                     <span v-show="patient.age_years != 0"> age: {{ patient.age_years }} years</span>
-                     <span v-show="patient.age_days != 0"> age: {{ patient.age_days }} days</span>
-                     <span v-show="patient.gestational_age != 0"> gestational age: {{ patient.gestational_age }} weeks</span>
+                     <span v-show="patient.age_years > 0"> age: {{ patient.age_years }} years</span>
+                     <span v-show="patient.age_days > 0"> age: {{ patient.age_days }} days</span>
+                     <span v-show="patient.gestational_age > 0"> gestational age: {{ patient.gestational_age }} weeks</span>
                   </span>
                 </button>
               </h2>
