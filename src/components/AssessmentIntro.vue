29b7fc0a0669b1e1bb202617d5597809a1820a02
David Herbert <david.herbert2@newcastle.ac.uk>
2026-06-15 11:03:01 +0100

    Add extra instructions to confirmation section

--- src/components/AssessmentIntro.vue
+++ src/components/AssessmentIntro.vue
@@ -24,6 +24,12 @@
         @click="setConfirmation"
       >I confirm all the above are the case
       </ButtonElement>
+      <StaticElement name="checklistPostamble">
+        <p>
+          It is recommended that users new to the ePRaSE tool try the <a href="/practice" target="_blank" title="Try practice mode in a new tab">practice mode (opens in new tab)</a> 
+          and watch the <a href="Javascript:alert('Need URL for video here')" title="Watch video in a new tab">instruction video (opens in new tab)</a> before proceedoing further.
+        </p>
+      </StaticElement>
     </GroupElement>
     <StaticElement v-if="requirementsConfirmed" name="introBody">
       <p>
